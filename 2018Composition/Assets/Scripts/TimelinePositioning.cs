06dd1f1a79f3ef447e2fea8c892044d82480dd12
Clayton Dembski <cjdembski@wpi.edu>
2018-06-08 20:18:27 -0400

    a quick scrolling script, and added a gitignore for all that metadata

--- 2018Composition/Assets/Scripts/TimelinePositioning.cs
+++ 2018Composition/Assets/Scripts/TimelinePositioning.cs
@@ -5,23 +5,41 @@ using UnityEngine.EventSystems;
 
 public class TimelinePositioning : MonoBehaviour{
 
-    //When the timeline is clicked and dragged, move it along the top of the timeline
+    private Vector3 screenPoint;
+    private Vector3 offset;
+		
+    void Start () {
+        Debug.Log("AHH");
+    }
+ 
+    void Update () {
+    }
+    
+    void OnMouseDown()
+    {
+        Debug.Log("Here");
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, screenPoint.y, screenPoint.z));
+    }
+    
     void OnMouseDrag(){
-        //transform mouse position to world position
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-          //if the mose is within the bounds of the timeline
-        if (mousePos.x > -1.7 && mousePos.x < 10.84)
+
+        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, screenPoint.y, screenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+        Debug.Log(cursorPosition.x);
+        
+        if (cursorPosition.x > -1.7 && cursorPosition.x < 10.84)
         {
-            transform.position = new Vector3(mousePos.x, transform.position.y, transform.position.z);
+            transform.position = cursorPosition;
         }
         //else snap to max or min
-        else if (mousePos.x <= -1.7)
+        else if (cursorPosition.x <= -1.7f)
         {
-            transform.position.x = new Vector3(1.7f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-1.7f, cursorPosition.y, cursorPosition.z);
         }
-        else if (mousePos.x >= 10.84)
+        else if (cursorPosition.x >= 10.84f)
         {
-            transform.position.x = new Vector3(10.84f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(10.84f, cursorPosition.y, cursorPosition.z);
         }
     }
 }
