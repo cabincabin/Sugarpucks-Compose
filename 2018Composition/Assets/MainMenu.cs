84e73ace15b0282020edfeee16b29150bfe49acd
Clifford Smith <cesmith2@wpi.edu>
2018-07-03 16:41:28 -0400

    Main Menu actually added.
    Will work on graphics/formatting learning screen later

---
+++ 2018Composition/Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void Compose()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public void Learn()
+	{
+		//SceneManager.LoadScene(//Still have to create this scene);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("QUIT!");
+		Application.Quit();
+	}
+
+}
