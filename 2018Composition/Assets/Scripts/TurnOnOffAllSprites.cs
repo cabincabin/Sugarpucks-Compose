8e2e26a2d02ed3f7b4677dadbca5c8782a1e2802
Clayton Dembski <cjdembski@wpi.edu>
2018-07-05 23:24:37 -0400

    added info!

---
+++ 2018Composition/Assets/Scripts/TurnOnOffAllSprites.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+    public class TurnOnOffAllSprites : MonoBehaviour
+    {
+        private bool OnOff;
+        public List<GameObject> turnSpriteOff;
+        
+        private void OnMouseUpAsButton()
+        {
+            if (OnOff)
+            {
+                foreach (var spriteGameObj in turnSpriteOff)
+                {
+                    spriteGameObj.gameObject.GetComponent<SpriteRenderer>().enabled = true;  
+                }
+                OnOff = false;
+
+            }
+            else
+            {
+                foreach (var spriteGameObj in turnSpriteOff)
+                {
+                    spriteGameObj.gameObject.GetComponent<SpriteRenderer>().enabled = false;  
+                }
+                OnOff = true;
+            }
+        }
+
+      
+    }
