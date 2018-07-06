f7b119017cc5acb29560cc6362b15485d7c4d5f5
Clayton Dembski <cjdembski@wpi.edu>
2018-07-05 20:41:01 -0400

    a finished homescreen

---
+++ 2018Composition/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	public Dropdown LearnDDown;
+	
+	public void Compose()
+	{
+		SceneManager.LoadScene(0);
+		Debug.Log(SceneManager.sceneCount);
+	}
+
+	public void Learn()
+	{
+		if(LearnDDown.value !=0)
+		SceneManager.LoadScene(LearnDDown.value);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("QUIT!");
+		Application.Quit();
+	}
+
+}
---
+++ 2018Composition/Assets/Scripts/SwitchBetweenTitles.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SwitchBetweenTitles : MonoBehaviour
+{
+    public ChangeSkin Skin;
+    private bool swap;
+    private int c100;
+    
+    private void Update()
+    {
+        if (c100 >= 50)
+        {
+            if (swap)
+            {
+                Skin.changeSkin(0);
+                swap = false;
+
+            }
+            else
+            {
+                Skin.changeSkin(1);
+                swap = true;
+            }
+
+            c100 = 0;
+        }
+        else
+        {
+            c100++;
+        }
+    }
+}
