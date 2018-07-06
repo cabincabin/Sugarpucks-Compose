00b0e742c7234e3b519d544d73bf43ca60d7c784
Clayton Dembski <cjdembski@wpi.edu>
2018-07-06 00:26:06 -0400

    changed some levels, Need to indicate numbers better, need to indicate beats

--- 2018Composition/Assets/Scripts/ChangeSkin.cs
+++ 2018Composition/Assets/Scripts/ChangeSkin.cs
@@ -5,6 +5,12 @@ public class ChangeSkin : MonoBehaviour
 {
     //0 is default skin
     public List<Sprite> Skins;
+    private Vector3 defaultSize;
+
+    private void Start()
+    {
+        defaultSize = transform.localScale;
+    }
 
     //0 is default
     public virtual void changeSkin(int SkinNum)
@@ -12,12 +18,13 @@ public class ChangeSkin : MonoBehaviour
         SpriteRenderer spriteRenderer = (SpriteRenderer)GetComponent<Renderer>();
         if (SkinNum > Skins.Count || SkinNum < 0)
         {
-
             spriteRenderer.sprite = Skins[0];
+            transform.localScale = new Vector3(defaultSize.x, defaultSize.y-.15f*SkinNum, defaultSize.z);
         }
         else
         {
             spriteRenderer.sprite = Skins[SkinNum];
+            transform.localScale = defaultSize;
         }
     }
 }
